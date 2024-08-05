3a064fbf67a8c1013f06efbb7736af2ecc8b6883
feiselk <feiselkemal15@gmail.com>
2024-08-05 15:03:27 -0700

    edited

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,24 @@
-#!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    breezy = 0
-    for i in range(x):
-        try:
-            if isinstance(my_list[i], x):
-                print("{:d}".format(my_list=[i]), end="")
-                breezy += 1
-        except (ValueError, TypeError):
-            pass
-    print()
-    return breezy
+    try:
+        count = 0
+        new_list = []
+        for i in range(len(my_list)):
+            if isinstance(my_list[i], int):
+                new_list.append(my_list[i])
+
+     
+        for i in range(x):
+            print("{:d}".format(new_list[i]), end = '')
+            count += 1
+
+        if i <= x:
+            print()
+            try:
+                if count > 5:
+                    raise IndexError
+            except IndexError:
+                print("IndexError: list index out of range")
+    except TypeError:
+        pass               
+        
+    return count
