ef473c1fa4b07ca018d8c32b90e2680a03678728
feiselk <feiselkemal15@gmail.com>
2024-08-06 08:06:28 -0700

    modified

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for i in range(x):
             if isinstance(my_list[i], int):
-            # Try to format and print the integer
+                # Try to format and print the integer
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
             else:
